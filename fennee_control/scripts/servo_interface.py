5cfbac6dab0bd614a7d69eb71aab9fa102520102
Matt Williamson <matt@aimatt.com>
2023-04-12 12:45:26 -0400

    Fixed angle calculations and it kind of walks

--- fennee_control/scripts/servo_interface.py
+++ fennee_control/scripts/servo_interface.py
@@ -16,8 +16,6 @@ SERVO_STATES_TOPIC = "servo_duty_cycles"
 JOINT_STATES_TOPIC = "joint_states"
 JOINT_CONTROLLER_TOPIC = "joint_group_position_controller/command"
 QUEUE_SIZE = 10
-RAD_TO_DEG = 57.2958
-DEGREES_PER_PWM = 180.0 / 285.0 # 285 pwm = 180 degrees
 
 
 import rospy
@@ -27,41 +25,92 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from sensor_msgs.msg import JointState
 from std_msgs.msg import UInt16MultiArray
 
+RAD_TO_DEG = 57.2958
+DEGREES_PER_PWM = 180.0 / 285.0 # 285 pwm / 180 degrees = 0.6315789474
+RADIANS_PER_PWM = math.pi / 285.0 # 0.01102313212
+
+# TODO: Publish joint angles to joint_state topic calculated from the pwm values
+# So we will show limits and everything
+
+# Sampled angles from controller        
+#   expected -> actual                                
+# sit pwm      stand pwm    center      sit angle    stand angle  
+# 400 -> 431   400 -> 431   400 -> 400  0.0000000   0.000000     # front_left_shoulder                                   
+# 351 -> 350   314 -> 309   207 -> 207  1.579998    1.132637     # front_left_leg                                    
+# 113 -> 166   256 -> 225   400 -> 400  -2.576207   -1.92721     # front_left_foot                                   
+# 297 -> 323   297 -> 323   297 -> 297  0.000000    -0.00000     # front_right_shoulder                                  
+# 260 -> 258   297 -> 299   402 -> 402  1.579998    1.132637     # front_right_leg                                   
+# 505 -> 447   362 -> 388   214 -> 214  -2.576207   -1.92721     # front_right_foot                                  
+# 328 -> 315   328 -> 315   328 -> 328  0.0000000   0.000000     # rear_left_shoulder                                    
+# 383 -> 381   347 -> 340   238 -> 238  1.579998    1.132637     # rear_left_leg                                 
+# 113 -> 168   256 -> 227   402 -> 402  -2.576207   -1.92721     # rear_left_foot                                    
+# 318 -> 309   318 -> 309   318 -> 318  0.0000000   -0.00000     # rear_right_shoulder                                   
+# 262 -> 262   299 -> 303   406 -> 406  1.579998    1.132637     # rear_right_leg                                    
+# 509 -> 456   366 -> 397   223 -> 223  -2.576207   -1.92721     # rear_right_foot                                   
+
+# Example: front left foot
+#
+#           sit,          stand,       center 
+#           ----          -----        ------
+#   pwm |   113.0,        256.0,       400.0
+# angle |   -2.576207,    -1.92721,    0.000000
+
+# DEGREES_PER_PWM = 180.0 / 285.0 # 285 pwm / 180 degrees = 0.6315789474
+# RADIANS_PER_PWM = math.pi / 285.0 # 0.01102313212
 
 # Manual calibration for now
 pwm_map = [
-    # min pwn, max pwm, sit pwm, stand pwm, sit angle, stand angle
-    (100.0,     900.0,   400.0,    400.0,   0.0000000,   0.000000),  # front_left_shoulder
-    (100.0,     900.0,   351.0,    314.0,   1.579998,    1.132637),  # front_left_leg
-    (100.0,     900.0,   113.0,    256.0,   -2.576207,   -1.92721),  # front_left_foot
-    (100.0,     900.0,   297.0,    297.0,   0.000000,    -0.00000),  # front_right_shoulder
-    (100.0,     900.0,   260.0,    297.0,   1.579998,    1.132637),  # front_right_leg
-    (100.0,     900.0,   505.0,    362.0,   -2.576207,   -1.92721),  # front_right_foot
-    (100.0,     900.0,   328.0,    328.0,   0.0000000,   0.000000),  # rear_left_shoulder
-    (100.0,     900.0,   383.0,    347.0,   1.579998,    1.132637),  # rear_left_leg
-    (100.0,     900.0,   113.0,    256.0,   -2.576207,   -1.92721),  # rear_left_foot
-    (100.0,     900.0,   318.0,    318.0,   0.0000000,   -0.00000),  # rear_right_shoulder
-    (100.0,     900.0,   262.0,    299.0,   1.579998,    1.132637),  # rear_right_leg
-    (100.0,     900.0,   509.0,    366.0,   -2.576207,   -1.92721),  # rear_right_foot
+    # min pwn, max pwm, center pwm, multiplier (reverse)
+    (100.0,     900.0,   431.0,   1.0), # front_left_shoulder
+    (100.0,     900.0,   213.0,   1.0), # front_left_leg
+    (100.0,     900.0,   399.0,   1.0), # front_left_foot
+    (100.0,     900.0,   323.0,   1.0), # front_right_shoulder
+    (100.0,     900.0,   391.0,  -1.0), # front_right_leg
+    (100.0,     900.0,   214.0,  -1.0), # front_right_foot
+    (100.0,     900.0,   315.0,  -1.0), # rear_left_shoulder
+    (100.0,     900.0,   245.0,   1.0), # rear_left_leg
+    (100.0,     900.0,   401.0,   1.0), # rear_left_foot
+    (100.0,     900.0,   309.0,  -1.0), # rear_right_shoulder
+    (100.0,     900.0,   398.0,  -1.0), # rear_right_leg
+    (100.0,     900.0,   222.0,  -1.0), # rear_right_foot
 ]
 
-def joint_state_to_pwm(angle, pwm_map_row):
-    min_pwm, max_pwm, sit_pwm, stand_pwm, sit_angle, stand_angle = pwm_map_row
-    angle_range = (stand_angle - sit_angle)
-    if angle_range == 0:
-        pwm = sit_pwm
-    else:
-        pwm = ( (angle - sit_angle) / angle_range) * (stand_pwm - sit_pwm) + sit_pwm
+
+# Center
+# data: [431, 207, 399, 323, 401, 214, 315, 238, 401, 309, 405, 223]
+# Moved shoulders out a bit
+# data: [406, 207, 399, 336, 401, 214, 335, 238, 401, 291, 405, 223]
+
+#  Standing All legs forward 180 degrees too far
+# data: [111, 511, 142, 515]
+
+# Centered and then moved just legs close to standing position
+# data: [271, 341, 310, 340]
+
+#Walking front legs are too close, back legs are too far, maybe 30 degrees too far
+# data: [457, 302, 237, 293, 292, 399, 341, 333, 239, 279, 296, 408]
+
+# Standing centered
+# data: [430, 309, 225, 322, 299, 388, 314, 340, 227, 308, 303, 397]
+
+
+
+
+
+def radians_to_pwm(angle, pwm_map_row):
+    min_pwm, max_pwm, center_pwm, multiplier = pwm_map_row
+    pwm = (angle / RADIANS_PER_PWM * multiplier) + center_pwm
     # Make sure the pwm is within the range
     if min_pwm > max_pwm:
         pwm = min(min_pwm, max(max_pwm, pwm))
     else:
         pwm = min(max_pwm, max(min_pwm, pwm))
-    return abs(int(pwm))
+    return int(pwm)
 
 def joint_states_to_pwms(angles):
     """Converts joint angles to pwm values"""
-    angles = [joint_state_to_pwm(a, pwm_map[i]) for i, a in enumerate(angles)]
+    # TODO: Can probably do a batch calculation for all servos with numpy
+    angles = [radians_to_pwm(a, pwm_map[i]) for i, a in enumerate(angles)]
     return angles
 
 
@@ -124,11 +173,59 @@ if __name__ == "__main__":
 
 
 def test():
-    from pprint import pprint
-    sitr = [x[4] for x in pwm_map]
-    pprint(joint_states_to_pwms(sitr))
-    standr = [x[5] for x in pwm_map]
-    pprint(joint_states_to_pwms(standr))
-    zeror = [-math.pi] * 12
-    pprint(joint_states_to_pwms(zeror))
+    sit_angles = [
+        0.0000000,
+        1.579998,
+        -2.576207,
+        0.000000,
+        1.579998,
+        -2.576207,
+        0.0000000,
+        1.579998,
+        -2.576207,
+        0.0000000,
+        1.579998,
+        -2.576207,
+    ]
+    stand_angles = [
+        0.000000,
+        1.132637,
+        -1.92721,
+        -0.00000,
+        1.132637,
+        -1.92721,
+        0.000000,
+        1.132637,
+        -1.92721,
+        -0.00000,
+        1.132637,
+        -1.92721,
+    ]
+    center_angles = [
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+    ]
+    print("")
+    print("Center")
+    for x in joint_states_to_pwms(center_angles):
+        print(x)
+    print("")
+    print("Sit")
+    for x in joint_states_to_pwms(sit_angles):
+        print(x)
+    print("")
+    print("Stand")
+    for x in joint_states_to_pwms(stand_angles):
+        print(x)
+    
     
