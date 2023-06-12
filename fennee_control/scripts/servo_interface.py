ddaa9280a319bbc3680fc900c0b47a5eaf58dcdc
Matt Williamson <matt@aimatt.com>
2023-06-12 15:35:13 +0200

    Making calibration script automated

--- fennee_control/scripts/servo_interface.py
+++ fennee_control/scripts/servo_interface.py
@@ -17,8 +17,7 @@ JOINT_CONTROLLER_TOPIC = "joint_group_position_controller/command"
 OUT_QUEUE_SIZE = 10
 IN_QUEUE_SIZE = 1
 PCA_PWM_FREQUENCY = 50 # Hz for analog servos
-# MAX_ANGLE = 270 # degrees
-MAX_ANGLE = 180 # degrees
+PCA_ADDRESS = 0x40
 
 import rospy
 # import numpy as np
@@ -28,125 +27,21 @@ from sensor_msgs.msg import JointState
 from std_msgs.msg import UInt16MultiArray
 from adafruit_servokit import ServoKit
 
-
-JOINT_NAMES = [
-    "front_left_shoulder",
-    "front_left_leg",
-    "front_left_foot",
-    # "front_left_toe",
-    "front_right_shoulder",
-    "front_right_leg",
-    "front_right_foot",
-    # "front_right_toe",
-    "rear_left_shoulder",
-    "rear_left_leg",
-    "rear_left_foot",
-    # "rear_left_toe",
-    "rear_right_shoulder",
-    "rear_right_leg",
-    "rear_right_foot",
-    # "rear_right_toe",
-]
-
-# TODO: Publish joint angles to joint_state topic calculated from the pwm values
-# So we will show limits and everything
-
-# Sampled angles from controller        
-#   expected -> actual                                
-# sit pwm      stand pwm    center      sit angle    stand angle  
-# 400 -> 431   400 -> 431   400 -> 400  0.0000000   0.000000     # front_left_shoulder                                   
-# 351 -> 350   314 -> 309   207 -> 207  1.579998    1.132637     # front_left_leg                                    
-# 113 -> 166   256 -> 225   400 -> 400  -2.576207   -1.92721     # front_left_foot                                   
-# 297 -> 323   297 -> 323   297 -> 297  0.000000    -0.00000     # front_right_shoulder                                  
-# 260 -> 258   297 -> 299   402 -> 402  1.579998    1.132637     # front_right_leg                                   
-# 505 -> 447   362 -> 388   214 -> 214  -2.576207   -1.92721     # front_right_foot                                  
-# 328 -> 315   328 -> 315   328 -> 328  0.0000000   0.000000     # rear_left_shoulder                                    
-# 383 -> 381   347 -> 340   238 -> 238  1.579998    1.132637     # rear_left_leg                                 
-# 113 -> 168   256 -> 227   402 -> 402  -2.576207   -1.92721     # rear_left_foot                                    
-# 318 -> 309   318 -> 309   318 -> 318  0.0000000   -0.00000     # rear_right_shoulder                                   
-# 262 -> 262   299 -> 303   406 -> 406  1.579998    1.132637     # rear_right_leg                                    
-# 509 -> 456   366 -> 397   223 -> 223  -2.576207   -1.92721     # rear_right_foot            
-#                        
-
-# Example: front left foot
-#
-#           sit,          stand,       center 
-#           ----          -----        ------
-#   pwm |   113.0,        256.0,       400.0
-# angle |   -2.576207,    -1.92721,    0.000000
-
-# Map the control list to PWM channels
-# No longer needed since the channels match the order of the joints
-# CHANNEL_MAP = [
-#     1,  # front_left_shoulder
-#     1,  # front_left_leg
-#     2,  # front_left_foot
-#     3,  # front_right_shoulder
-#     4,  # front_right_leg
-#     5,  # front_right_foot
-#     6,  # rear_left_shoulder
-#     7,  # rear_left_leg
-#     8,  # rear_left_foot
-#     9, # rear_right_shoulder
-#     10, # rear_right_leg
-#     11,  # rear_right_foot
-# ]
-
-
-# Manual calibration for now
-PWM_MAP = [
-    # center pwm, multiplier (reverse)
-    
-    (94,  1.0),    # 0 front_left_shoulder
-    (37,   1.0),    # 1 front_left_leg
-    (151,  1.0),    # 2 front_left_foot
-    (95,   1.0),   # 3 front_right_shoulder
-    (155,  -1.0),   # 4 front_right_leg
-    (38,  -1.0),    # 5 front_right_foot
-    (90,  -1.0),   # 6 rear_left_shoulder
-    (53,   1.0),    # 7 rear_left_leg
-    (154,  1.0),    # 8 rear_left_foot
-    (80,  -1.0),    # 9 rear_right_shoulder
-    (157, -1.0),    # 10 rear_right_leg
-    (46,  -1.0),    # 11 rear_right_foot
-]
-
-# 0 230 # front_left_foot
-# 1 45 # front_left_leg
-# 2 250 # front_left_shoulder
-# 3 60 # front_right_foot
-# 4 240 # front_right_leg
-# 5 160 # front_right_shoulder
-# 6 230 # rear_left_foot
-# 7 90 # rear_left_leg
-# 8 150 # rear_left_shoulder
-# 9 70 # rear_right_foot
-# 10 220 # rear_right_leg
-# 11 120 # rear_right_shoulder
-
-
-def radians_to_pwm(angle, pwm_map_row):
-    # TODO: Subtract from 360 if negative
-    center_pwm, multiplier = pwm_map_row
-    pwm = (math.degrees(angle) * multiplier) + center_pwm
-    if pwm < 0:
-        pwm += 360
-    pwm = int(pwm % 360)
-    pwm = min(MAX_ANGLE, pwm)
-    return pwm
-
-def joint_states_to_pwms(angles):
-    """Converts joint angles to pwm values"""
-    # TODO: Can probably do a batch calculation for all servos with numpy
-    pwms = [radians_to_pwm(a, PWM_MAP[i]) for i, a in enumerate(angles)]
-    # remapped = [pwms[i] for i in CHANNEL_MAP] # No longer needed since the channels match
-    return pwms
-
-
 class ServoInterface:
     def __init__(self):
+        super().__init__()
+
+        self.servo_calibration = rospy.get_param("servo_calibration")
+        self.servo_calibration_file = rospy.get_param("servo_calibration_file")
+        self.hardware_connected = rospy.get_param("hardware_connected")
+        self.joint_names = rospy.get_param("/hardware_interface/joints")
+        self.max_angle = rospy.get_param("servo_max_angle")
+
         # PCA9685 I2C PWM controller
-        self.pwm = ServoKit(channels=16)
+        if self.hardware_connected:
+            self.pwm = ServoKit(channels=16, address=PCA_ADDRESS) # , frequency=PCA_PWM_FREQUENCY
+        else:
+            rospy.loginfo("hardware_connected is false. Don't actually connect to I2C")
         # for i in range(0, 12):
         #     self.pwm.servo[i].actuation_range = MAX_ANGLE
         self.joint_positions = None
@@ -166,10 +61,49 @@ class ServoInterface:
         )
         rospy.Timer(rospy.Duration(1.0 / PUBLISH_FREQUENCY), self.publish_positions)
 
+    def servo_calibration_to_pwm_map(self):
+        """Converts the servo calibration to a pwm map"""
+        pwm_map = []
+        for joint in self.joint_names:
+            center_pwm = self.servo_calibration[joint]["angle"]
+            multiplier = self.servo_calibration[joint]["multiplier"]
+            pwm_map.append((center_pwm, multiplier))
+        return pwm_map
+
+    def check_connection(self):
+        rospy.loginfo("Checking hardware connection")
+        if not self.hardware_connected:
+            rospy.loginfo("Hardware connection disabled")
+            return
+        try:
+            self.pwm.servo[0].angle
+        except OSError:
+            rospy.logerr("ServoInterface: Failed to connect to PCA9685")
+            self.hardware_connected = False
+        rospy.loginfo("Looks good")
+
+    def radians_to_pwm(self, angle, pwm_map_row):
+        # TODO: Subtract from 360 if negative
+        center_pwm, multiplier = pwm_map_row["angle"], pwm_map_row["multiplier"]
+        pwm = (math.degrees(angle) * multiplier) + center_pwm
+        if pwm < 0:
+            pwm += 360
+        pwm = int(pwm % 360)
+        pwm = min(self.max_angle, pwm)
+        return pwm
+
+    def joint_states_to_pwms(self, angles):
+        """Converts joint angles to pwm values"""
+        # TODO: Can probably do a batch calculation for all servos with numpy
+        pwms = [self.radians_to_pwm(angles[i], self.servo_calibration[a]) for i, a in enumerate(self.get_joint_names())]
+        # remapped = [pwms[i] for i in CHANNEL_MAP] # No longer needed since the channels match
+        return pwms
+
     def handle_joint_commands(self, data: JointTrajectory):
         # rospy.loginfo("handle_joint_commands")
+        # rospy.loginfo(data)
         self.joint_positions = data
-        angles = joint_states_to_pwms(data.points[0].positions)
+        angles = self.joint_states_to_pwms(data.points[0].positions)
         self.set_servo_positions(angles)
 
 
@@ -189,15 +123,16 @@ class ServoInterface:
         angles = cmd.positions
         # print(self.get_joint_names())
         # print(dict(zip(self.get_joint_names(), angles)))
-        angles = joint_states_to_pwms(angles)
+        angles = self.joint_states_to_pwms(angles)
         msg = UInt16MultiArray(data=tuple(int(abs(a)) for a in angles))
         self.servo_states_topic.publish(msg)
 
     def set_servo_positions(self, angles):
         # rospy.loginfo("set_servo_positions")
         # rospy.loginfo(angles)
-        for i, angle in enumerate(angles):
-            self.pwm.servo[i].angle = angle
+        if self.hardware_connected:
+            for i, angle in enumerate(angles):
+                self.pwm.servo[i].angle = angle
 
     def publish_joint_state(self, names, cmd: JointTrajectoryPoint):
         msg = self.msg
@@ -208,71 +143,14 @@ class ServoInterface:
 
     def get_joint_names(self):
         # return self.joint_positions.joint_names
-        return JOINT_NAMES
+        return self.joint_names
 
 
 if __name__ == "__main__":
     rospy.loginfo("Starting servo_interface...")
     servo_interface = ServoInterface()
+    servo_interface.check_connection()
     rospy.loginfo("servo_interface started.")
     while not rospy.is_shutdown():
         rospy.spin()
 
-
-def test():
-    sit_angles = [
-        0.0000000,
-        1.579998,
-        -2.576207,
-        0.000000,
-        1.579998,
-        -2.576207,
-        0.0000000,
-        1.579998,
-        -2.576207,
-        0.0000000,
-        1.579998,
-        -2.576207,
-    ]
-    stand_angles = [
-        0.000000,
-        1.132637,
-        -1.92721,
-        -0.00000,
-        1.132637,
-        -1.92721,
-        0.000000,
-        1.132637,
-        -1.92721,
-        -0.00000,
-        1.132637,
-        -1.92721,
-    ]
-    center_angles = [
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-    ]
-    print("")
-    print("Center")
-    for x in joint_states_to_pwms(center_angles):
-        print(x)
-    print("")
-    print("Sit")
-    for x in joint_states_to_pwms(sit_angles):
-        print(x)
-    print("")
-    print("Stand")
-    for x in joint_states_to_pwms(stand_angles):
-        print(x)
-    
-    
